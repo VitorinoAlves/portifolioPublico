cf87aa8487213387718314357d0e2e65a315c032
VitorinoAlves <vitorino.17alexandre@gmail.com>
2024-11-25 18:32:44 -0300

    Criando temas

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
+import { ThemeProvider } from 'styled-components';
+
 import Projetos from './containers/Projetos';
 import Sidebar from './containers/Sidebar';
 import Sobre from './containers/Sobre';
 import EstiloGlobal, { Contianer } from './styles';
+import temaLight from './themes/light';
+import temaDark from './themes/dark';
+import { useState } from 'react';
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false);
+
+  function trocaTema(){
+    setEstaUsandoTemaDark(!estaUsandoTemaDark);
+  }
+
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal></EstiloGlobal>
       <Contianer>
-        <Sidebar></Sidebar>
+        <Sidebar trocaTema={trocaTema}></Sidebar>
         <main>
           <Sobre></Sobre>
           <Projetos></Projetos>
         </main>
       </Contianer>
 
-    </>
+    </ThemeProvider>
 
   );
 }
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -4,7 +4,11 @@ import Titulo from "../../components/Titulo";
 
 import { Descricao, BotaoTema, SidebarContainer } from "./styles";
 
-const Sidebar = () => (
+type Props = {
+  trocaTema: () => void;
+}
+
+const Sidebar = (props: Props) => (
   <aside>
     <SidebarContainer>
       <Avatar></Avatar>
@@ -13,7 +17,7 @@ const Sidebar = () => (
       <Descricao tipo="principal" fontSize={12}>
         Engenheiro de qualidade
       </Descricao>
-      <BotaoTema>Trocar tema</BotaoTema>
+      <BotaoTema onClick={props.trocaTema}>Trocar tema</BotaoTema>
     </SidebarContainer>
 
   </aside>
